3aa74cdd85cd74ebbd40430f5202992d2c3d483b
alexeyselin <brawler80@mail.ru>
2022-12-17 16:04:41 +0300

    version 1.0.1

---
+++ lib/bloc/bloc.dart
@@ -0,0 +1,52 @@
+ import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:test_task/bloc/state.dart';
+import 'package:test_task/repository/repository.dart';
+
+import 'event.dart';
+
+class LoginBloc extends Bloc<LoginEvent, LoginState> {
+  LoginBloc(this.repository) : super(const PhoneInputState());
+
+  final LoginRepository repository;
+   late String _phone;
+
+  @override
+  Stream<LoginState> mapEventToState(
+    LoginEvent event,
+  ) async* {
+
+
+    if (event is PhoneEnteredEvent){
+      _phone = event.phone;
+      yield const StateIsLoading();
+      try {
+        await repository.requestSms(_phone);
+        yield  SmsRequestedState (_phone);
+      } catch (e) {
+        yield   PhoneInputState(error: e.toString());
+
+      }
+      }
+    else if (event is ReenterPhoneEvent) {
+      yield const PhoneInputState();
+    }
+    else if (event is ResendSmsEvent) {
+      yield SmsRequestedState(_phone);
+    }
+    else if (event is CheckEnteredCode) {
+      final code = event.code;
+      yield const StateIsLoading();
+        try {
+        await repository.checkCode(_phone, code);
+        yield  const LoginSuccessState ();
+      } catch (e) {
+        yield   SmsRequestedState(_phone, error: e.toString());
+
+      }
+    }
+
+    }
+
+    //TODO: describe logic
+  }
+
---
+++ lib/bloc/state.dart
@@ -0,0 +1,27 @@
+import 'package:equatable/equatable.dart';
+
+abstract class LoginState extends Equatable {
+  const LoginState({this.error, this.isLoading = false});
+
+  final String? error;
+  final bool isLoading;
+
+  @override
+  List<Object?> get props => [error, isLoading];
+}
+class StateIsLoading extends LoginState {
+  const StateIsLoading ({isLoading}) : super (isLoading:true);
+}
+class PhoneInputState extends LoginState {
+  const PhoneInputState({String? error}) : super(error: error);
+}
+
+class SmsRequestedState extends LoginState {
+  const SmsRequestedState(this.phone, {String? error}) : super(error: error);
+
+  final String phone;
+}
+
+class LoginSuccessState extends LoginState {
+  const LoginSuccessState() : super();
+}
